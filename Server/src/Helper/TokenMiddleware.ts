30daa6b34526110f4b2bd7c5cbc13ba897a66824
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-08-06 23:06:37 +0530

    verify token Done

--- Server/src/Helper/TokenMiddleware.ts
+++ Server/src/Helper/TokenMiddleware.ts
@@ -2,10 +2,23 @@ import jwt from "jsonwebtoken"
 import createResponse from "./CreateResponse";
 
 const middlewareToken = (req:any,res:any,next:any)=>{
-    const header = req.token.authorization;
+    const header = req.headers.authorization;
     if(!header){
         return createResponse(res,false,400,"Token not found",[],true);
     }
-    const token
+    const token = header.split(" ")[1];
+    try {
+        jwt.verify(token,process.env.JWT_SECRET_TOKEN as string,(err:any,decode:any)=>{
+            if(err){
+                return createResponse(res,false,400,"Invalid Token",[],true);
+            }
+            req.user = decode;
+            next();
+        });
+    }catch (error:any){
+        return createResponse(res,false,500,error.message,[],true);
+    }
 
 }
+
+export default middlewareToken
