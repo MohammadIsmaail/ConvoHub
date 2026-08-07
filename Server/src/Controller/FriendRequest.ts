5ed649f3c13b18c14ec082db79ca50045f7ebb55
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-08-07 16:23:47 +0530

    working on friend request list module

--- Server/src/Controller/FriendRequest.ts
+++ Server/src/Controller/FriendRequest.ts
@@ -28,7 +28,6 @@ export const FriendRequestController = async (req:any,res:any)=>{
         return createResponse(res,false,500,error.message,[],true);
     }
 }
-
 export const GetPendingRequestsController = async (req:any,res:any)=>{
     try{
 
@@ -47,4 +46,87 @@ export const GetPendingRequestsController = async (req:any,res:any)=>{
         return createResponse(res, false, 500, error.message, [], true);
     }
 }
+export const AcceptFriendRequestController = async (req:any,res:any)=>{
+    try{
+        const { id } = req.user;
+        const { requestId } = req.body;
 
+        const request = await FriendRequestModel.findById(requestId);
+
+        //  Request exist karti hai?
+        if(!request){
+            return createResponse(res, false, 404, "Friend request not found", [], true);
+        }
+
+        // 5. Check karo login user hi receiver hai
+        // request.receiver === id ?
+        if(request.receiver.toString() !== id){
+            return createResponse(res, false, 403, "You are not authorized to accept this friend request", [], true);
+        }
+        else{
+            if(request.status   !== "pending"){
+                return createResponse(res, false, 400, "Friend request already Proecssed", [], true);
+            }
+                request.status = "accepted";
+                await request.save();
+                return createResponse(res, true, 200, "Friend request accepted successfully", request, false);
+        }
+
+    }catch(error:any){
+        return createResponse(res, false, 500, error.message, [], true);
+    }
+}
+export const RejectFriendRequestController = async (req:any,res:any)=>{
+    try{
+        const { id } = req.user;
+        const { requestId } = req.body;
+
+        const request = await FriendRequestModel.findById(requestId);
+
+        // 4. Request exists?
+        if(!request){
+            return createResponse(res, false, 404, "Friend request not found", [], true);
+        }
+        else{
+            if(request.receiver.toString() !== id){
+                return createResponse(res, false, 403, "You are not authorized to reject this friend request", [], true);
+            }
+            if(request.status !== "pending"){
+                return createResponse(res, false, 400, "Friend request already processed", [], true);
+            }
+            request.status = "rejected";
+            await request.save();
+            return createResponse(res, true, 200, "Friend request rejected successfully", request, false);
+        }   
+
+    }catch(error:any){
+        return createResponse(res, false, 500, error.message, [], true);
+    }
+}
+
+export const FriendsListController = async (req:any,res:any)=>{
+    try{
+
+        // 1. Login user id
+        const { id } = req.user;
+
+        // 2. Accepted requests find karo
+        // sender = id OR receiver = id
+        // status = accepted
+
+        // 3. Populate sender
+        // 4. Populate receiver
+
+        // 5. Friends array banao
+
+        // 6. Login user ko remove karke
+        // doosre user ko friend banao
+
+        // 7. Response return
+
+    }catch(error:any){
+
+        // Error response
+
+    }
+}
--- Server/src/Router/UserRouter.ts
+++ Server/src/Router/UserRouter.ts
@@ -2,10 +2,14 @@ import express from "express";
 import { UserLoginController, UserRegisterController } from "../Controller/userauth";
 import { UserProfileController } from "../Controller/userRelatedData";
 import middlewareToken from "../Helper/TokenMiddleware";
+import { AcceptFriendRequestController, FriendRequestController, GetPendingRequestsController } from "../Controller/FriendRequest";
 const userRouter = express.Router();
 
 userRouter.post("/register",UserRegisterController);
 userRouter.post("/login",UserLoginController);
 userRouter.get("/profile", middlewareToken, UserProfileController);
+userRouter.post("/friend-request/send", middlewareToken, FriendRequestController);
+userRouter.get("/friend-requests", middlewareToken, GetPendingRequestsController);
+userRouter.post("/friend-request/accept", middlewareToken, AcceptFriendRequestController);
 
 export default userRouter;
