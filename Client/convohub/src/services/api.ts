edca7954231e18b727c31a2c9f6c5143acec297f
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-08-14 23:34:39 +0530

    Done api interceptor

--- Client/convohub/src/services/api.ts
+++ Client/convohub/src/services/api.ts
@@ -6,6 +6,16 @@ const api = axios.create({
         "Content-Type":"application/json"
     }
 })
-
-api.interceptor
+//  interceptor ka use isliye karte hai ki sath main token pass hota rahe
+api.interceptors.request.use((config)=>{
+     const token = localStorage.getItem("token")
+     if(token){
+        config.headers.Authorization = `Bearer ${token}`
+     }
+     return config
+},
+(error)=>{
+    return Promise.reject(error)
+}
+)
 export default api
