d77d42fda023faf8f8745096cf8c40f9b1af0210
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-08-17 00:37:29 +0530

    improvise conversation Slice

--- Client/convohub/src/redux/slices/conversationSlice.ts
+++ Client/convohub/src/redux/slices/conversationSlice.ts
@@ -1,29 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    selectedConversation:null,
-    messages:[] as any[]
-}
+    selectedConversation: null,
+    messages: [] as any[],
+};
 
 const conversationSlice = createSlice({
-    name:"conversation",
+    name: "conversation",
     initialState,
-    reducers:{
-        setSelectedConversation:(state,action)=>{
-            state.selectedConversation=action.payload
+    reducers: {
+        setSelectedConversation: (state, action) => {
+            state.selectedConversation = action.payload;
         },
-        setMessages:(state,action)=>{
-            state.messages=action.payload
-        },
-        addMessage:(state,action)=>{
-            state.messages.push(action.payload)
-        },
-        clearMessages:(state)=>{
-            state.messages=[]
-        }
 
-    }
-})
+        setMessages: (state, action) => {
+            state.messages = action.payload;
+        },
 
-export const {setSelectedConversation,setMessages,addMessage,clearMessages} = conversationSlice.actions
-export default conversationSlice.reducer
+        addMessage: (state, action) => {
+            state.messages.push(action.payload);
+        },
+
+        updateMessageSeen: (state, action) => {
+            state.messages = state.messages.map((msg) =>
+                msg._id === action.payload
+                    ? { ...msg, seen: true }
+                    : msg
+            );
+        },
+
+        clearMessages: (state) => {
+            state.messages = [];
+        },
+
+        clearConversation: (state) => {
+            state.selectedConversation = null;
+            state.messages = [];
+        },
+    },
+});
+
+export const {
+    setSelectedConversation,
+    setMessages,
+    addMessage,
+    updateMessageSeen,
+    clearMessages,
+    clearConversation,
+} = conversationSlice.actions;
+
+export default conversationSlice.reducer;
